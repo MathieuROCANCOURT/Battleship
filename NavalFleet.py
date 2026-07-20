188cb79c869d6e17d8a3231b51301af2aedee45c
MathieuROCANCOURT <mathieu.rocancourt@fms-ea.com>
2026-07-20 15:45:02 +0200

    fix: Modify boolean value from True to False in __init__ method.

--- NavalFleet.py
+++ NavalFleet.py
@@ -5,14 +5,14 @@ from Boat import Boat
 
 
 class NavalFleet:
-    def __init__(self, naval_fleet=list[Boat] | None):
+    def __init__(self, naval_fleet: list[Boat] | None = None):
         if naval_fleet is None:
             naval_fleet = [
-                Boat({(2, 2): True, (2, 3): True, (2, 4): True, (2, 5): True, (2, 6): True}),
-                Boat({(4, 1): True, (5, 1): True, (6, 1): True, (7, 1): True}),
-                Boat({(5, 3): True, (6, 3): True, (7, 3): True}),
-                Boat({(5, 8): True, (5, 9): True, (5, 10): True}),
-                Boat({(9, 5): True, (9, 6): True})
+                Boat({(2, 2): False, (2, 3): False, (2, 4): False, (2, 5): False, (2, 6): False}),
+                Boat({(4, 1): False, (5, 1): False, (6, 1): False, (7, 1): False}),
+                Boat({(5, 3): False, (6, 3): False, (7, 3): False}),
+                Boat({(5, 8): False, (5, 9): False, (5, 10): False}),
+                Boat({(9, 5): False, (9, 6): False})
             ]
         self.naval_fleet = naval_fleet
 
