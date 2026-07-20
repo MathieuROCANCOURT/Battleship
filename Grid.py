73ab871272582765d3c0c321e3f75ebeb46d2190
MathieuROCANCOURT <mathieu.rocancourt@fms-ea.com>
2026-07-20 13:38:30 +0200

    feat: Create __repr__ and modify __init__ in Cell class

--- Grid.py
+++ Grid.py
@@ -4,14 +4,25 @@
 from Cell import Cell
 
 
-class Grid():
+class Grid:
     def __init__(self, size: int = 10):
         self.size = size
+        self.grid_game = [[Cell() for _ in range(self.size)] for _ in range(self.size)]
 
-    def init_grid(self):
-        """
-        Generate start grid
+    def __repr__(self):
+        seperate_line = ('+' + '-' * 3) * (self.size + 1) + "+\n"
+        output_grid = ' ' * 4 + seperate_line[4:] + ' ' * 4 + '|'
 
-        :return: grid with size x size
-        """
-        return [[Cell() for _ in range(self.size)] for _ in range(self.size)]
+        for index_col in range(self.size):
+            output_grid += ' ' + chr(index_col + ord('A')) + ' ' + '|'
+        output_grid += '\n'
+
+        for index_row_grid in range(1, self.size + 1):
+            output_grid += seperate_line + '|' + ' ' * (2 - len(str(index_row_grid))) + str(index_row_grid) + ' |'
+
+            for index_column_grid in range(self.size):
+                output_grid += self.grid_game[index_row_grid - 1][index_column_grid]
+            output_grid += '\n'
+
+        output_grid += seperate_line
+        return output_grid
