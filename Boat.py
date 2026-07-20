0cd1e258c258c37b7bedfc43db504434a3c3cf86
MathieuROCANCOURT <mathieu.rocancourt@fms-ea.com>
2026-07-20 14:54:46 +0200

    feat: Add is_shoot_down method.

--- Boat.py
+++ Boat.py
@@ -8,5 +8,8 @@ from dataclasses import dataclass
 class Boat:
     dict_coordinate_boats: dict[tuple[int, int], bool]
 
-    def get_ship_coord(self):
+    def get_ship_coord(self) -> list[tuple[int, int]]:
         return list(self.dict_coordinate_boats.keys())
+
+    def is_shoot_down(self) -> bool:
+        return any(list(self.dict_coordinate_boats.values()))
